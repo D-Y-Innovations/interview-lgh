a0650db9b27a6693e82ae6be5f5973f6cb940b12
marko36 <47961908+marko36@users.noreply.github.com>
2019-02-26 21:45:26 +0800

    timu

---
+++ async.ts
@@ -0,0 +1,35 @@
+﻿import superagent = require('http');
+import cheerio = require('cheerio');
+
+
+const remote_get =function(url: string) {
+ const promise = new Promise<superagent.Response>(function(resolve,reject){
+	http.get(url)
+		.end(function(err,res){
+			if(!err){
+				resolve(res);
+			}else{
+				console.log(err);
+				reject(err);
+			}
+		});
+	});
+	return promise;
+}
+let result =0;
+let count=11;
+let sum=0;
+
+async function crawl(){
+	for(let nums=1;nums<11;nums++){
+	let res = await remote_get('http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/'+nums+'.txt');
+	var a =res.text;
+	a =a.replace((/\s/g,"");
+        let bluefile = a.match(/\d+(\\.\\d+){0}/);
+	 if(bluefile!=null){
+            result+=parseInt(bluefile[0]);
+        }  
+    }
+    console.log("结果"+result);
+}
+crawl();
---
+++ quicksort.ts
@@ -0,0 +1,37 @@
+var quickSort = function (arr, left, right) {
+
+  if (right <= left) {
+	return;
+}
+
+  
+  left = 0;
+  right = arr.length - 1;
+
+  var leftPoint = left;
+  var rightPoint = right;
+  var temp = arr[left];
+
+  while (leftPoint != rightPoint) {
+
+    while (arr[rightPoint] >= temp && leftPoint < rightPoint) {
+      rightPoint--;
+    }
+    while (arr[leftPoint] <= temp && leftPoint < rightPoint) {
+      leftPoint++;
+    }
+    if (leftPoint < rightPoint) {
+      var changeNumber = arr[leftPoint];
+      arr[leftPoint] = arr[rightPoint];
+      arr[rightPoint] = changeNumber;
+    }
+  }
+
+  arr[left] = arr[leftPoint];
+  arr[leftPoint] = temp;
+
+  quickSort(arr, left, leftPoint - 1)
+  quickSort(arr, leftPoint + 1, right)
+
+  return arr
+};
