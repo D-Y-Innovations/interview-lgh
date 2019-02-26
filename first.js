84493f5e93dab9496ba0fc28a3f77fa77ce0dffd
marko36 <47961908+marko36@users.noreply.github.com>
2019-02-26 18:13:43 +0800

    Create first.js
    不知道做的对不对

---
+++ first.js
@@ -0,0 +1,43 @@
+﻿const http = require('http');
+const cheerio = require('cheerio');
+
+var add = function(time){
+	return new Promise(function(resolve,reject){
+		setTimeout(function() {
+var urls = ['http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/1.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/2.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/3.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/4.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/5.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/6.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/7.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/8.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/9.txt',
+'http://dy-public.oss-cn-shenzhen.aliyuncs.com/interviewTestData/10.txt'];
+
+for(var i=0;i<urls.length;i++){
+    http.get(urls[i],function(res){
+	var html = '';
+	var sum =0;
+	res.on('data',function(data){
+	html+=data;
+				     });
+	res.on('end',function(){
+	var $ = cheerio.load(html);
+	var count = $('#data>def').text().trim();
+		sum+=count;
+				});
+ 				   });
+				}//for循环结束
+		
+		resolve(sum);
+					},time);
+						    })
+};
+
+async function start(){
+	let result = await add(2000);
+	console.log(result);
+};
+
+start();
